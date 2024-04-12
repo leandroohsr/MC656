8147e23d633d7d08284cbede379e1e53c3a7ca57
eoleleu <l213437@dac.unicamp.br>
2024-04-11 22:44:41 -0300

    feat: :construction: criando a primeira parte da landing page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 
 import { Disclosure } from "@headlessui/react";
 import { Link } from "react-router-dom";
-
+import imgLogoHome from "../assets/logo.png";
 const Navbar = () => {
   const navigation = [
     "Página 1",
@@ -10,7 +10,9 @@ const Navbar = () => {
     "Página 4",
     "Página 5",
   ];
-
+  
+  const routes = ["/login", "/home", "/"];
+  
   return (
     <div className="w-full bg-black">
       <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
@@ -19,7 +21,10 @@ const Navbar = () => {
           {({ open }) => (
             <>
               <div className="flex flex-wrap items-center justify-between w-full lg:w-auto">
-                <Link to="/">
+                <Link  className="flex " to="/">
+                  <div className="w-6 mr-4 -ml-20"> 
+                    <img src={imgLogoHome} alt="Logo" />
+                  </div>
                   <span className="flex items-center space-x-2 text-2xl font-medium dark:text-gray-100">
                     {/* <span>
                       <img
@@ -57,16 +62,17 @@ const Navbar = () => {
                   </svg>
                 </Disclosure.Button>
 
-
+                {/* menu que aparece apenas quando a tela está pequena  */}
                 <Disclosure.Panel className="flex flex-wrap w-full my-5 lg:hidden">
                   <>
                     {navigation.map((item, index) => (
-                      <Link key={index} to="/" className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:focus:bg-gray-800 focus:outline-none">
+                      <Link key={index} to={routes[index]} className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:focus:bg-gray-800 focus:outline-none">
                           {item}
+                          {index}
                       </Link>
                     ))}
-                    <Link to="/" className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-600 rounded-md lg:ml-5">         
-                        Get Started
+                    <Link to="/" className="w-full px-6 py-2 mt-3 text-center text-white rounded-md bg-amber-500 lg:ml-5">         
+                        Login
                     </Link>
                   </>
                 </Disclosure.Panel>
@@ -75,12 +81,12 @@ const Navbar = () => {
           )}
         </Disclosure>
 
-        {/* menu  */}
+
         <div className="hidden text-center lg:flex lg:items-center">
           <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
             {navigation.map((menu, index) => (
               <li className="mr-3 nav__item" key={index}>
-                <Link to="/" className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-amber-500 focus:text-amber-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
+                <Link to={routes[index]}  className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-amber-500 focus:text-amber-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
                     {menu}
                 </Link>
               </li>
@@ -89,7 +95,7 @@ const Navbar = () => {
         </div>
 
         <div className="hidden mr-3 space-x-4 lg:flex nav__item">
-          <Link to="/login" className="px-6 py-2 text-black rounded-md bg-amber-500 md:ml-5">
+          <Link to="/login" className="px-6 py-2 font-medium text-white rounded-md bg-amber-500 md:ml-5">
               Login
           </Link>
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,10 @@
 import Navbar from "../components/NavBar";
+import Hero from "../components/Hero";
 export const HomePage = () => {
     return (
         <>
         <Navbar />
+        <Hero />
         <h1 className="text-3xl font-bold underline">
             Hello world!
         </h1>
