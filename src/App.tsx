0601e59567d112c7a05e2fb092cce0c5935b145e
ra248333 <h248333@dac.unicamp.br>
2024-04-18 19:40:40 -0300

    cabo a bateria

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginPage } from './pages/LoginPage';
 import { HomePage } from './pages/HomePage';
+import { CalendarioPage } from './pages/CalendarioPage';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/calendario" element={<CalendarioPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/CalendarioPage.tsx
@@ -0,0 +1,28 @@
+export const CalendarioPage = () => {
+    
+
+    return (
+        
+            <div className="grid grid-cols-6 gap-6 align-items: center justify-content: center">
+                <div>01</div>
+                <div>02</div>
+                <div>03</div>
+                <div>04</div>
+                <div>05</div>
+                <div>06</div>
+                <div>07</div>
+                <div>08</div>
+                <div>09</div>
+                <div>10</div>
+                <div>11</div>
+                <div>12</div>
+                <div>13</div>
+                <div>14</div>
+                <div>15</div>
+                <div>16</div>
+                <div>17</div>
+                <div>18</div>
+                <div>19</div>
+            </div>
+    );
+}
