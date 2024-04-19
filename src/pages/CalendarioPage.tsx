b83f75815ac6bf46d4f167c052764995d899d953
Matheus Mantovani Meneghel <m230906@dac.unicamp.br>
2024-04-18 21:04:59 -0300

    Começo do calendario

--- src/pages/CalendarioPage.tsx
+++ src/pages/CalendarioPage.tsx
@@ -1,28 +1,94 @@
+import { useState } from "react";
+
 export const CalendarioPage = () => {
     
+    const[showPopup, setShowPopup] = useState(false);
+
+    const alternPopup = () => {
+        setShowPopup(!showPopup)
+    }
 
     return (
-        
-            <div className="grid grid-cols-6 gap-6 align-items: center justify-content: center">
-                <div>01</div>
-                <div>02</div>
-                <div>03</div>
-                <div>04</div>
-                <div>05</div>
-                <div>06</div>
-                <div>07</div>
-                <div>08</div>
-                <div>09</div>
-                <div>10</div>
-                <div>11</div>
-                <div>12</div>
-                <div>13</div>
-                <div>14</div>
-                <div>15</div>
-                <div>16</div>
-                <div>17</div>
-                <div>18</div>
-                <div>19</div>
+            <div className="grid grid-cols-7 h-screen">
+                <button data-modal-target="default-modal" data-modal-toggle="default-modal" className="border border-black text-black"> Dia 1</button>
+                <button onClick={alternPopup} className="border border-black text-black"> Dia 2</button>
+                {showPopup && (<Popup props = {showPopup}></Popup>)}
+                <button className="border border-black text-black"> Dia 3</button>
+                <button className="border border-black text-black"> Dia 4</button>
+                <button className="border border-black text-black"> Dia 5</button>
+                <button className="border border-black text-black"> Dia 6</button>
+                <button className="border border-black text-black"> Dia 7</button>
+                <button className="border border-black text-black"> Dia 8</button>
+                <button className="border border-black text-black"> Dia 9</button>
+                <button className="border border-black text-black"> Dia 10</button>
+                <button className="border border-black text-black"> Dia 11</button>
+                <button className="border border-black text-black"> Dia 12</button>
+                <button className="border border-black text-black"> Dia 13</button>
+                <button className="border border-black text-black"> Dia 14</button>
+                <button className="border border-black text-black"> Dia 15</button>
+                <button className="border border-black text-black"> Dia 16</button>
+                <button className="border border-black text-black"> Dia 17</button>
+                <button className="border border-black text-black"> Dia 18</button>
+                <button className="border border-black text-black"> Dia 19</button>
             </div>
+
     );
 }
+
+function Popup(props: any) {
+
+    const alternPopup = () => {
+        props.setShowPopup(!props.showPopup)
+    }
+
+    return (
+        <>
+            {
+                (
+                    <div id="default-modal" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                        <div className="relative p-4 w-full max-w-2xl max-h-full">
+                            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                    <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                                        Terms of Service
+                                    </h3>
+                                    <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                        </svg>
+                                        <span className="sr-only">Close modal</span>
+                                    </button>
+                                </div>
+                                <div className="p-4 md:p-5 space-y-4">
+                                    <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                        With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                                    </p>
+                                    <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                        The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                                    </p>
+                                </div>
+                                <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                                    <button data-modal-hide="default-modal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                                    <button data-modal-hide="default-modal" type="button" className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Decline</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        </>
+    )
+}
+
