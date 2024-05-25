6a304e6438aadb24766bde9fd79955a3fd4b2b22
arturrur <a224538@dac.unicamp.br>
2024-05-25 20:19:51 -0300

    fix(smellCode data)

--- src/pages/CalendarioPage.tsx
+++ src/pages/CalendarioPage.tsx
@@ -13,20 +13,20 @@ import {
   ArrowLeftIcon
 } from "@heroicons/react/24/solid";
 
-interface SportEvent{
+interface SportEvent {
   Dia: any;
   Modalidade: any;
   Horario: any;
   Local: any;
   Evento: any;
-  Gênero: any;
+  Genero: any;
 }
 
 export const CalendarioPage = () => {
 
   const [showPopup, setShowPopup] = useState(false);
 
-  const[data, setData] = useState<SportEvent[]>([]);
+  const [data, setData] = useState<SportEvent[]>([]);
 
   const Data = require('../assets/Dados/dados.csv');
 
@@ -54,14 +54,12 @@ export const CalendarioPage = () => {
   data.forEach((event) => {
     const date = event.Dia;
     const existingIndex = arrays.findIndex((arr) => arr[0]?.Dia === date);
-    if (existingIndex !== -1){
+    if (existingIndex !== -1) {
       arrays[existingIndex].push(event);
-    } else { 
+    } else {
       arrays.push([event])
     }
   });
-
-    console.log(arrays)
 
   const arrays_sports: string[][] = arrays.map((dayEvents) => {
     const sportsSet = new Set<string>();
@@ -70,8 +68,6 @@ export const CalendarioPage = () => {
     })
     return Array.from(sportsSet);
   })
-
-  console.log(arrays_sports)
 
   const dias = [
     "24 de Julho",
@@ -94,6 +90,27 @@ export const CalendarioPage = () => {
     "11 de Agosto",
   ];
 
+  const [esporte, setEsporte] = useState('')
+  const [index, setIndex] = useState(-1)
+  const [showDetalhes, setShowDetalhes] = useState(false)
+
+  const [eventos, setEventos] = useState([])
+
+  function mudarEvento(index: number, esporte: string) {
+    let vetor: any = []    
+    vetor = arrays[index].filter((evento) =>
+      evento.Modalidade === esporte
+    )
+    setEventos(vetor)
+  }
+
+  function detalharEvento(esporte: string, index: number, showDetalhes: boolean) {
+    setEsporte(esporte)
+    setIndex(index)
+    setShowDetalhes(showDetalhes)
+    mudarEvento(index, esporte)
+  }
+
   function alternPopup(str: string, info: string, loc: string, time: string) {
     if (document.getElementById("default-modal")!.style.display === "block") {
       document.getElementById("default-modal")!.style.display = "none";
@@ -109,13 +126,13 @@ export const CalendarioPage = () => {
     setCurrentIndex(index);
   }
 
-  function voltarDia(){
-    if(currentIndex>0){
+  function voltarDia() {
+    if (currentIndex > 0) {
       setCurrentIndex(currentIndex - 1)
     }
   }
-  function avancarDia(){
-    if (currentIndex<17){
+  function avancarDia() {
+    if (currentIndex < 17) {
       setCurrentIndex(currentIndex + 1)
     }
   }
@@ -145,8 +162,8 @@ export const CalendarioPage = () => {
                 <p className="mb-4 text-gray-500 dark:text-gray-400">Programação das competições olímpicas:</p>
                 <ul className="mb-4 space-y-4">
                   <div>
-                    {arrays_sports[currentIndex].map((item, index) => (
-                      <li>
+                    {arrays_sports[currentIndex].map((item) => (
+                      <li onClick={() => detalharEvento(item, currentIndex, true)}>
                         <label className="inline-flex items-center justify-between w-full p-5 mb-4 text-gray-900 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-500 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-900 hover:bg-gray-100 dark:text-white dark:bg-gray-600 dark:hover:bg-gray-500">
                           <div className="block">
                             <div className="w-full text-lg font-semibold">{item}</div>
@@ -161,9 +178,47 @@ export const CalendarioPage = () => {
             </div>
           </div>
         </div> : null}
-        <div onClick={()=>{        window.location.href = '/';}} className="flex w-16 ml-4 rounded-lg shadow-lg hover:underline ">
-          <ArrowLeftIcon />
+      {showDetalhes == true ?
+        <div id="default-modal" aria-hidden="true" className="fixed flex items-center justify-center w-full h-full max-h-full overflow-x-hidden overflow-y-auto bg-opacity-50 md:inset-0">
+          <div className="w-full max-w-md max-h-full p-4 ">
+            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+              <div className="flex items-center justify-between p-4 border-b rounded-t md:p-5 dark:border-gray-600">
+                <h3 className="flex-grow text-lg font-semibold text-center text-gray-900 dark:text-white">
+                  {dias[currentIndex]}
+                </h3>
+                <button onClick={() => setShowDetalhes(false)} type="button" className="inline-flex items-center justify-center w-8 h-8 text-sm text-gray-400 bg-transparent rounded-lg hover:bg-gray-200 hover:text-gray-900 ms-auto dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                  <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                  </svg>
+                  <span className="sr-only">Close modal</span>
+                </button>
+              </div>
+              <div className="p-4 md:p-5">
+                <p className="mb-4 text-gray-500 dark:text-gray-400">Mostrando os eventos de {esporte}:</p>
+                <ul className="mb-4 space-y-4">
+                  <div>
+                    {eventos.map((item: any) => (
+                      <li>
+                        <label className="inline-flex items-center justify-between w-full p-5 mb-4 text-gray-900 bg-white border border-gray-200 rounded-lg dark:hover:text-gray-300 dark:border-gray-500 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-900 hover:bg-gray-100 dark:text-white dark:bg-gray-600 dark:hover:bg-gray-500">
+                          <div className="block">
+                            <div className="w-full text-lg font-semibold">{item.Horario}</div>
+                            <div className="w-full text-lg font-semibold">{item.Evento}</div>
+                            <div className="w-full text-lg font-semibold">{item.Genero}</div>
+                          </div>
+                          {/* <div className="w-4 h-4 text-gray-500 ms-3 rtl:rotate-180 dark:text-gray-400" aria-hidden="true">{item.icon}</div> */}
+                        </label>
+                      </li>
+                    ))}
+                  </div>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
+        : null}
+      <div onClick={() => { window.location.href = '/'; }} className="flex w-16 ml-4 rounded-lg shadow-lg hover:underline ">
+        <ArrowLeftIcon />
+      </div>
       <div className="container pt-10 mx-auto bg-gray-200 pb-96">
 
         <div className="w-full bg-white rounded shadow wrapper ">
@@ -185,7 +240,7 @@ export const CalendarioPage = () => {
                 <td onClick={() => handlePopUp(0)} className="w-10 h-40 p-1 overflow-auto transition duration-500 border border-white cursor-pointer xl:w-40 lg:w-30 md:w-30 sm:w-20 ease hover:bg-gray-300">
                   <div className="flex flex-col w-10 h-40 mx-auto overflow-hidden xl:w-40 lg:w-30 md:w-30 sm:w-full">
                     <div className="w-full h-full bg-gray-200 rounded-lg shadow-lg">
-                    <span className="font-bold text-gray-500 ">{dias[0]}</span>
+                      <span className="font-bold text-gray-500 ">{dias[0]}</span>
                       <div className="w-full h-full ">
                         <div className="grid grid-cols-4 gap-4 m-4">
                           <div className=""><CalendarDaysIcon /></div>
