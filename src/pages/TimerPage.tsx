f1adb9aa6c66d8b8dc3af351addf7a76e17d8363
Matheus Mantovani Meneghel <23090@dac.unicamp.br>
2024-06-05 12:09:00 -0300

    Fix code smell timer!

--- src/pages/TimerPage.tsx
+++ src/pages/TimerPage.tsx
@@ -1,19 +1,32 @@
 export const TimerPage = () => {
+    const calcTime = (tempo_miliseg: number) => {
+        var segundos_total = Math.floor(tempo_miliseg / 1000);
+        var minutos_total = Math.floor(segundos_total / 60);
+        var horas_total = Math.floor(minutos_total / 60);
+
+        var dias = Math.floor(horas_total / 24) + 1;
+        var horas = horas_total % 24;
+        var minutos = minutos_total % 60;
+        var segundos = segundos_total % 60;
+
+        return [dias, horas, minutos, segundos];
+    };
+
     const timer = () => {
+        // Define data do começo das olimpíadas
         var dataAlvo: Date = new Date('2024-07-26');
         dataAlvo.setHours(14,30,0);
-        const dataAtual: Date = new Date();
-        const tempo_miliseg: number = dataAlvo.getTime() - dataAtual.getTime();
-        // vo transformar pra horas, minutos e segundos, da pra colocar isso em uma função
-        const segundos_total = Math.floor(tempo_miliseg / 1000);
-        const minutos_total = Math.floor(segundos_total / 60);
-        const horas_total = Math.floor(minutos_total / 60);
-        const dias = Math.floor(horas_total / 24) + 1;
-        const segundos = segundos_total % 60;
-        const minutos = minutos_total % 60;
-        const horas = horas_total % 24;
+
+        // Checa data atual
+        var dataAtual: Date = new Date();
+
+        // Calcula diferença das datas
+        var tempo_miliseg: number = dataAlvo.getTime() - dataAtual.getTime();
+
+        // Calcula os dias/horas/minutos/segundos da diferença
+        var [dias, horas, minutos, segundos] = calcTime(tempo_miliseg);
+
         console.log(`${dias}:${horas}:${minutos}:${segundos}`);
-        // console.log(dataAtual.toString());
     };
     setInterval(timer, 1000);
 
